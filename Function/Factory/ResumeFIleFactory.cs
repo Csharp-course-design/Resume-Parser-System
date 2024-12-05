b79212c6037cce1cba11c3989206519207def0f3
03xiaoyuhe <2297622003@qq.com>
2024-12-05 17:32:23 +0800

    修改了工厂逻辑

--- Function/Factory/ResumeFIleFactory.cs
+++ Function/Factory/ResumeFIleFactory.cs
@@ -1,22 +1,22 @@
 ﻿using Function;
 using Models;
 
-namespace CsharpAPI.Factory
+namespace Function.Factory
 {
     public class ResumeFIleFactory : IFactory
     {
         /// <summary>
         /// 生成文件的Models类
         /// </summary>
-        /// <param name="Path">文件路径</param>
+        /// <param name="FilePath">文件路径</param>
         /// <returns></returns>
-        public static object Get(string Path)
+        public static object Get(string FilePath)
         {
             // path 改为 只获取文件名 
             return new ResumeFile(
                 0,
-                Path,
-                Base64Helper.FileToBase64String(Path),
+                Path.GetFileName(FilePath),
+                Base64Helper.FileToBase64String(FilePath),
                 DateTime.Now
                 );
         }
